c5aa7aca23e268dfcd8d90045368cbb026c0da0a
ctong5 <cassandra.tong5@gmail.com>
2019-08-09 22:48:59 -0700

    Setup basic directory structure

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const style = require('style-loader');
+const css = require('css-loader');
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
+              import: true,
+              importLoaders: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
