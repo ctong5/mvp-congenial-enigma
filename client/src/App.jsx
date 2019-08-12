76b1aec242669c216b3fb7c6f818d4762c679d61
ctong5 <cassandra.tong5@gmail.com>
2019-08-11 18:10:29 -0700

    Fetch data from mapbox and hikingproject APIs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Landing from './Landing';
 import NavBar from './NavBar';
 import Signup from './Signup';
@@ -9,10 +10,18 @@ class App extends React.Component {
     super(props);
     this.state = {
       page: 'home',
+      searchTerm: '',
+      searchLat: 0,
+      searchLon: 0,
+      hikeResults:'',
     };
     this.handleClickSignup = this.handleClickSignup.bind(this);
     this.handleClickHome = this.handleClickHome.bind(this);
     this.handleClickLogin = this.handleClickLogin.bind(this);
+    this.addUser = this.addUser.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
+    this.searchHikingProject = this.searchHikingProject.bind(this);
   }
 
   handleClickSignup() {
@@ -33,12 +42,51 @@ class App extends React.Component {
     });
   }
 
+  handleSearch(e) {
+    this.setState({
+      searchTerm: e.target.value,
+    });
+  }
+
+  searchHikingProject(lat, lon, token) {
+    axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&key=${token}`)
+    .then(responseHiking => {
+      console.log(responseHiking);
+      this.setState ({
+        hikeResults: responseHiking.data.trails,
+      });
+    })
+  }
+
+  submitSearch(e, searchTerm) {
+    e.preventDefault();
+    const token = this.props.searchMapbox;
+    axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.searchTerm}.json?access_token=${token}`)
+    .then(responseMapbox => {
+      console.log('response from mapbox', responseMapbox);
+      this.setState ({
+        searchLon: responseMapbox.data.features[0].geometry.coordinates[0],
+        searchLat: responseMapbox.data.features[0].geometry.coordinates[1],
+      });
+    })
+    .then(() => this.searchHikingProject(this.state.searchLat, this.state.searchLon, this.props.searchHikingProject))
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
   render() {
     let currentview;
     if (this.state.page === 'signup') {
-      currentview = <Signup />
+      currentview = <Signup addUser={this.addUser}/>
     } else if (this.state.page === 'home') {
-      currentview = <Landing />
+      currentview = (
+        <Landing 
+          handleSearch={this.handleSearch} 
+          submitSearch={this.submitSearch} 
+          searchTerm={this.state.searchTerm}
+        />
+      )
     } else if (this.state.page === 'login') {
       currentview = <Login />
     }
@@ -46,7 +94,11 @@ class App extends React.Component {
     return(
       <div>
         <div>
-          <NavBar handleClickSignup={this.handleClickSignup} handleClickHome={this.handleClickHome} handleClickLogin={this.handleClickLogin}/>
+          <NavBar 
+            handleClickSignup={this.handleClickSignup} 
+            handleClickHome={this.handleClickHome} 
+            handleClickLogin={this.handleClickLogin}
+          />
         </div>
   
         <div>
--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const Landing = () => {
+const Landing = (props) => {
+  const {
+    handleSearch,
+    submitSearch,
+    searchTerm
+  } = props;
 
   return(
     <div>
       <p>This is the landing page...</p>
       <h2>It's a beautiful day. Go outside!</h2>
       <form>
-        <input type="text" placeholder="Where to next?" />
-        <input type="submit" value="Submit" />
+        <input type="text" placeholder="Where to next?" onChange={(e)=>handleSearch(e)}/>
+        <input type="submit" value="Submit" onClick={(e, searchTerm)=>submitSearch(e, searchTerm)}/>
       </form>
     </div>
   )
