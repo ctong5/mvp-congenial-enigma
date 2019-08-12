4e547d66417effb66103146a7aed6e25b7b3c48e
ctong5 <cassandra.tong5@gmail.com>
2019-08-11 19:01:55 -0700

    Pass in API keys as props to App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
 import App from './App';
+import HIKINGPROJECT_API_KEY from '../config/hikingproject.js';
+import MAPBOX_API_KEY from '../config/mapbox.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App searchHikingProject={HIKINGPROJECT_API_KEY} searchMapbox={MAPBOX_API_KEY} />, document.getElementById('root'));
