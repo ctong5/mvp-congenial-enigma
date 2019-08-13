42f7f44ce0f22058d4e30848a28aaa3626e040b9
ctong5 <cassandra.tong5@gmail.com>
2019-08-12 19:40:00 -0700

    Add signup page

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 
-const Signup = () => {
-  return (
-    <div>
-      <form>
-        <input type="text" name="firstname" placeholder="Firstname" />
-        <input type="text" name="lastname" placeholder="Lastname" />
-        <input type="text" name="email" placeholder="Email" /> 
-        <input type="text" name="password" placeholder="Password" />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  )
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitSignup = this.submitSignup.bind(this);
+  }
+
+  handleChange(e) {
+    console.log("handlechange firstname: ", e.target.name)
+    console.log("id: ", e.target.id)
+    console.log("value: ", e.target.value)
+    // e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  // function to pass state to handleSubmitSignup
+  submitSignup(e) {
+    this.props.addUser(e, this.state);
+  }
+
+  render() {
+    return (
+      <div className="signup">
+        <div className="signupWelcomeForm">
+          <div>Welcome, let's get signed up!</div>
+          <form>
+            <input type="text" name="email" placeholder="Email" onChange={(e) => this.handleChange(e)} /> 
+            <input type="text" name="password" placeholder="Password" onChange={(e) => this.handleChange(e)}/>
+            {/*<input type="submit" value="Sign Up" onClick={this.props.handleSubmitSignup}/>*/}
+            <button onClick={(e) => this.submitSignup(e)}>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+  }  
 };
 
 export default Signup;
