d274881557ada99cf0eb18a732fd567a1682613b
ctong5 <cassandra.tong5@gmail.com>
2019-08-30 00:13:33 -0700

    Rename component from Landing to SearchHikes

--- client/src/SearchHikes.jsx
+++ client/src/SearchHikes.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import HikeList from './HikeList';
 
-const Landing = (props) => {
+const SearchHikes = (props) => {
   const {
     handleSearch,
     submitSearch,
-    searchTerm
+    searchTerm,
+    hikeResults,
+    currentUser,
   } = props;
 
   return(
     <div>
-      <h2>It's a beautiful day. Find your next outdoor adventure...</h2>
+      <h2>Welcome {currentUser}!</h2>
+      <h4>It's a beautiful day. Find your next outdoor adventure...</h4>
       <form>
         <input type="text" placeholder="Where to next?" value={searchTerm} onChange={(e)=>handleSearch(e)}/>
         <input type="submit" value="Submit" onClick={(e, searchTerm)=>submitSearch(e, searchTerm)}/>
       </form>
+
+      <div>
+        <HikeList hikeResults={hikeResults} />
+      </div>
     </div>
   )
 };
 
-export default Landing;
+export default SearchHikes;
