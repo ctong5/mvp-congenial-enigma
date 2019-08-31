202054e98d7c01bbe9321ba188bd51c0ce2423e9
ctong5 <cassandra.tong5@gmail.com>
2019-08-31 00:40:52 -0700

    Update greeting headline

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <h2>
-      It's a beautiful day. Find your next outdoor adventure
-    </h2>
-  </div>
+  <header>
+    <div className="homeContainer">
+      <h2>It's a beautiful day.</h2>
+      <h3>Find your next outdoor adventure</h3>
+    </div>
+  </header>
 )
 
 export default Home;
