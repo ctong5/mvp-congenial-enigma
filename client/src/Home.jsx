b96bb55707bc89d1e6c0690bafe5986e4852bb52
ctong5 <cassandra.tong5@gmail.com>
2019-09-02 10:18:13 -0700

    Remove header element

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Home = () => (
-  <header>
-    <div className="homeContainer">
-      <h2>It's a beautiful day.</h2>
-      <h3>Find your next outdoor adventure</h3>
-    </div>
-  </header>
+  <div className="homeContainer">
+    <h2>It's a beautiful day.</h2>
+    <h3>Find your next outdoor adventure</h3>
+  </div>
 )
 
 export default Home;
