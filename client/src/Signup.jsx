d8be4bff2d94d2323baceb67473b34797d392808
ctong5 <cassandra.tong5@gmail.com>
2019-09-03 11:46:49 -0700

    Add email and password requirements

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -33,11 +33,11 @@ class Signup extends React.Component {
       <div className='signupContainer'>
         <div className='signupGreetingForm'>
           <div className='signupGreeting'>WELCOME!<br/>LET'S GET SIGNED UP</div>
-          <form className='signupForm'>
-            <input type="text" name="email" placeholder="email" value={this.state.email} onChange={(e) => this.handleChange(e)} /> 
-            <input type="password" name="password1" placeholder="password" value={this.state.password1}onChange={(e) => this.handleChange(e)}/>
-            <input type="password" name="password2" placeholder="retype password" value={this.state.password2}onChange={(e) => this.handleChange(e)}/>
-            <button onClick={(e) => this.submitSignup(e)}>Sign Up</button>
+          <form className='signupForm' onSubmit={(e) => this.submitSignup(e)}>
+            <input type="email" name="email" placeholder="email" required value={this.state.email} onChange={(e) => this.handleChange(e)} /> 
+            <input type="password" name="password1" placeholder="password" minLength="8" required value={this.state.password1} onChange={(e) => this.handleChange(e)}/>
+            <input type="password" name="password2" placeholder="retype password" minLength="8" required value={this.state.password2} onChange={(e) => this.handleChange(e)}/>
+            <button>Sign Up</button>
           </form>
         </div>
       </div>
